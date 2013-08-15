b8062e6c3d44da1017071bdf9b3fa719a19d879b
Adrian Rodriguez <oglowo@gmail.com>
2013-08-14 19:26:29 -0700

    Working on the xml marshalling for callfirexml

---
+++ src/main/scala/com/oglowo/callfire/callfirexml/CallFireXmlTag.scala
@@ -0,0 +1,12 @@
+package com.oglowo.callfire.callfirexml
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: adrian
+ * Date: 8/14/13
+ * Time: 6:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+trait CallFireXmlTag {
+
+}
---
+++ src/main/scala/com/oglowo/callfire/callfirexml/DialPlan.scala
@@ -0,0 +1,12 @@
+package com.oglowo.callfire.callfirexml
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: adrian
+ * Date: 8/14/13
+ * Time: 6:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class DialPlan {
+
+}
